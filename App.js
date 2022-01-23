4ab945a72a20148f66062a8652d874cbdd14de9a
nerdstarcode <sthivendev@gmail.com>
2022-01-23 16:44:02 -0300

    base visual pronta

--- App.js
+++ App.js
@@ -1,9 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Task from './components/Task'
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <View style={styles.tasksWrapper}>
+        <Text style={styles.sectiontitle}>Today's tasks</Text>
+        <View style={styles.items}>
+        <Task text={'Text 1'}/>
+        <Task text={'Text 2'}/>
+        </View>
+      </View>
     </View>
   );
 }
@@ -11,6 +19,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#E8EAED',
+  },
+  tasksWrapper:{
+    paddingTop: 80,
+    paddingHorizontal: 20,
+  },
+  sectiontitle:{
+    fontSize:24,
+    fontWeight: 'bold',
+  },
+  items:{
+    marginTop: 30,
   },
 });
